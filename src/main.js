90f1a0416a114315de0706ca9ead0d296ad7a54c
Cabanga <joao.cabanga@digitalfactory.co.ao>
2019-04-11 12:11:24 +0100

    update de web app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from '@/services/store'
 import './registerServiceWorker'
+
+import firebase from 'firebase/app';
+import config from '@/services/firebase';
+
+
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+let app;
+
+if (!app) {
+  app = new Vue({
+    router,
+    store,
+    render: h => h(App),
+  }).$mount('#app');
+}
--- src/router.js
+++ src/router.js
@@ -1,27 +1,89 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
 
-Vue.use(Router)
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Dasboard from './views/Dasboard.vue';
+import Login from './views/Login.vue';
 
-export default new Router({
+import firebase from 'firebase/app';
+
+
+Vue.use(Router);
+
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About,
+    },
+
+    {
+      path: '/dasboard',
+      name: 'dasboard',
+      component: Dasboard,
+      meta: {
+        requiresAuth: true
       }
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+
+
+  ],
+});
+
+// ========================== Nav Guard ==========================
+router.beforeEach((to, from, next) => {
+  //store.state.currentUser
+
+  if ( to.matched.some(record => record.meta.requiresAuth) ) {
+    // check if user exist
+    if (! firebase.auth().currentUser ) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    }else{
+      //Proced to de router
+      next();
     }
-  ]
-})
+  }else if ( to.matched.some(record => record.meta.requiresGuest) ) {
+    // check if user exist
+    if ( firebase.auth().currentUser ) {
+      next({
+        path: '/dasboard',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    }else{
+      //Proced to de router
+      next();
+    }
+  }else{
+    //Proced to de router
+    next();
+  }
+
+});
+
+export default router;
