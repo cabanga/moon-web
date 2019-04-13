1f4da312a9f4acbe6114f0bee1627789219f2cdc
Cabanga <joao.cabanga@digitalfactory.co.ao>
2019-04-13 21:43:49 +0100

    add card vacancy

---
+++ src/components/VacancyCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="col-sm-4">
+        <div class="card card-moon">
+            
+            <div class="card-title">
+                <br>
+                <h5 class="text-center color-default">{{vacancy.title}}</h5>
+            </div>
+            <div class="card-body">
+                <p><span>{{vacancy.company}}</span> </p>
+                <p class="p-info"><strong>Salário : </strong> <span>{{vacancy.salary}}</span> </p>
+                <p class="p-info"><strong>Local de trabalho : </strong> <span>{{vacancy.workPlace}}</span> </p>
+                <p class="p-info"><strong>Cidade : </strong> <span>{{vacancy.city}}</span> </p>
+                <br>
+                <button type="submit" class="btn btnSubmit-login btn-sm ">Ver detalhes</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            vacancy: Object
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,11 @@
   <div>
     <div class="bg-home">
       <div class="container container-home">
-        <h2 class="text-center text-title">Plataforma 100% Angolana, para vagas de T.I.</h2> 
-
+        <h2 class="text-center text-title ">
+          Plataforma 100% Angolana, para vagas de T.I.
+        </h2> 
       </div>
     </div>
-
-
 
     <div class="container">
       <br>
@@ -16,52 +15,33 @@
       <br>
       <br>
       <br>
-      
-      <h2>Viva Angola</h2> 
-      <br>   
-      <h2>Bom dia Angola</h2>    
-      <h2>Bom dia Angola</h2>
+      <div class="row no-padding">
+        <VacancyCard v-for="vac in dataVacancies" :vacancy="vac" :key="vac.id">
+        </VacancyCard>
+      </div>
+      <br>
+      <br>
     </div>
-     
   </div>
 </template>
 
 <script>
+  /* eslint func-call-spacing: ["error", "never"] */
+  /* eslint function-paren-newline: ["error", "multiline"] */
+  /* eslint-disable */
+  
+  import VacancyCard from '@/components/VacancyCard.vue';
+  import dataV from '@/api/data.json'
+
   export default {
     name: 'home',
+    data () {
+      return {
+        dataVacancies: dataV
+      }
+    },
+    components: {
+      VacancyCard
+    }
   }
 </script>
-
-
-<style>
-  .text-title{
-    color: #271637;
-    padding: 40px 0px;
-  }
-
-  .container-home{
-    position: relative;
-    background-color: #ffffff;
-    height: 200px;
-    bottom: -81%;
-    width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
-    box-shadow: #00000038 0px -70px 74px;
-  }
-
-  .bg-home {
-    background-attachment: scroll;
-    background-color: #666;
-    background-image: url('../assets/images/header.jpg');
-    background-position: top left, center center;
-    background-repeat: repeat, no-repeat;
-    background-size: auto, cover;
-    color: #fff!important;
-    height: 500px;
-    margin-top: -3%;
-  }
-
-</style>
