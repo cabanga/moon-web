7f28ff7f1b6e1cfd298a4b5b46b2c3437c184989
Cabanga <joao.cabanga@digitalfactory.co.ao>
2019-04-13 12:29:49 +0100

    add box show at home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,67 @@
 <template>
-  <div class="home">
+  <div>
+    <div class="bg-home">
+      <div class="container container-home">
+        <h2 class="text-center text-title">Plataforma 100% Angolana, para vagas de T.I.</h2> 
+
+      </div>
+    </div>
+
+
+
+    <div class="container">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      
+      <h2>Viva Angola</h2> 
+      <br>   
+      <h2>Bom dia Angola</h2>    
+      <h2>Bom dia Angola</h2>
+    </div>
+     
   </div>
 </template>
 
 <script>
-
-
-export default {
-  name: 'home',
-
-}
+  export default {
+    name: 'home',
+  }
 </script>
+
+
+<style>
+  .text-title{
+    color: #271637;
+    padding: 40px 0px;
+  }
+
+  .container-home{
+    position: relative;
+    background-color: #ffffff;
+    height: 200px;
+    bottom: -81%;
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    box-shadow: #00000038 0px -70px 74px;
+  }
+
+  .bg-home {
+    background-attachment: scroll;
+    background-color: #666;
+    background-image: url('../assets/images/header.jpg');
+    background-position: top left, center center;
+    background-repeat: repeat, no-repeat;
+    background-size: auto, cover;
+    color: #fff!important;
+    height: 500px;
+    margin-top: -3%;
+  }
+
+</style>
