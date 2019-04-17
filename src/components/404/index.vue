182b3eca521d05898bcfadcb1324e5764178c772
Cabanga <joao.cabanga@digitalfactory.co.ao>
2019-04-17 09:36:07 +0100

    update page 404

--- src/components/404/index.vue
+++ src/components/404/index.vue
@@ -27,178 +27,187 @@
 </template>
 
 <style scoped>
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-*::before,
-*::after {
-    content: '';
-  position: absolute;
-}
-
-body{
-	background: #1B0034;
-	background-image: linear-gradient( 135deg, #1B0034 10%, #33265C 100%);
-	background-attachment: fixed;
-	background-size: cover;
-
-}
-
-.error {
-	width: 100%;
-	height: auto;
-	text-align: center;
-	margin-bottom: 0;
-}
-
-.dracula{
-	width: 230px;
-	height: 300px;
-	display: inline-block;
-	margin: auto;
-	overflowX: hidden;
-}
-
-.error .p {
-	height: 100%;
-	color: #b63c06;
-	font-size: 280px;
-	margin: 50px;
-	display: inline-block;
-	font-family: 'Anton', sans-serif;
-	font-family: 'Combo', cursive;
-}
-
-.con{
-  width: 500px;
-  height: 500px;
-  position: relative;
-  margin: 9% auto 0;
-  animation: ani9 0.7s ease-in-out infinite  alternate ;
-}
-
-@keyframes ani9 {
-  0%{
-    transform: translateY(10px);
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
 
-  100%{
-    transform: translateY(30px);
+  *::before,
+  *::after {
+      content: '';
+    position: absolute;
   }
-}
 
-.con > * {
-  position: absolute;
-  top: 0; left: 0;
-}
+  body{
+    background: #1B0034;
+    background-image: linear-gradient( 135deg, #1B0034 10%, #33265C 100%);
+    background-attachment: fixed;
+    background-size: cover;
+  }
 
-.hair{
-  top: -20px;
-  width: 210px;
-  height: 200px;
-  background: #C0D7DD;
-  border-radius: 0 50% 0 50%;
-  transform: rotate(45deg);
-  background: #b63c06;
-}
+  .error {
+    width: 100%;
+    height: auto;
+    text-align: center;
+    margin-bottom: 0;
+  }
 
-.hair-r{
-  left: 20px;
-  width: 210px;
-  height: 200px;
-  background: #C0D7DD;
-  border-radius: 0 50% 0 50%;
-  transform: rotate(45deg);
-  background: #b63c06;
-}
+  .dracula{
+    width: 230px;
+    height: 300px;
+    display: inline-block;
+    margin: auto;
+    overflowX: hidden;
+  }
 
-.head {
-  width: 200px;
-  height: 200px;
-  background: #C0D7DD;
-  border-radius: 0 50% 0 50%;
-  transform: rotate(45deg);
-}
+  .error .p {
+    height: 100%;
+    color: #b63c06;
+    font-size: 280px;
+    margin: 50px;
+    display: inline-block;
+    font-family: 'Anton', sans-serif;
+    font-family: 'Combo', cursive;
+  }
 
-.eye {
- width: 20px; height:20px;
-  background: #111113;
-  border-radius: 50%;
-  top: 15%; left: 11.5%;
-  transition: .3s linear;
-}
+  .con{
+    width: 500px;
+    height: 500px;
+    position: relative;
+    margin: 9% auto 0;
+    animation: ani9 0.7s ease-in-out infinite  alternate ;
+  }
 
-.eye-r{left: 24%;}
+  @keyframes ani9 {
+    0%{
+      transform: translateY(10px);
+    }
+    100%{
+      transform: translateY(30px);
+    }
+  }
 
-.mouth {
-  width: 60px;
-  height: 20px;
-  background: #840021;
-  top: 20%;
-  left: 14%;
-  border-radius: 50% / 0 0 100% 100%;
-}
+  .con > * {
+    position: absolute;
+    top: 0; left: 0;
+  }
 
-.mouth::after{
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-top: 13px solid #FFFFFF;
-  left: 10px;
-}
+  .hair{
+    top: -20px;
+    width: 210px;
+    height: 200px;
+    background: #C0D7DD;
+    border-radius: 0 50% 0 50%;
+    transform: rotate(45deg);
+    background: #b63c06;
+  }
 
-.mouth::before{
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-top: 13px solid #FFFFFF;
-  left: 40px;
-}
+  .hair-r{
+    left: 20px;
+    width: 210px;
+    height: 200px;
+    background: #C0D7DD;
+    border-radius: 0 50% 0 50%;
+    transform: rotate(45deg);
+    background: #b63c06;
+  }
 
-.blod {
-  width: 8px;
-  height: 20px;
-  background: #840021;
-  top: 23%; left: 17%;
-  border-radius: 20px;
-}
+  .head {
+    width: 200px;
+    height: 200px;
+    background: #C0D7DD;
+    border-radius: 0 50% 0 50%;
+    transform: rotate(45deg);
+  }
 
-.blod::after{
-  width: 2px;
-  height: 10px;
-  background: #FFF;
-  top: 20%; left: 10%;
-  border-radius: 20px;
-}
+  .eye {
+    width: 20px; height:20px;
+    background: #111113;
+    border-radius: 50%;
+    top: 15%; left: 11.5%;
+    transition: .3s linear;
+  }
 
-.blod2 {
-  top: 23%; left: 20%;
-  width: 13px;
-  height: 13px;
-  border-radius: 50% 50% 50% 0;
-  transform: rotate(130deg);
-  animation: blod 2s linear infinite;
-  opacity: 0;
-}
+  .eye-r{
+    left: 24%;
+  }
 
-@keyframes blod {
-  0%   {opacity: 1;}
-  100%   {background:red; opacity: 0; top:50%;}
-}
+  .mouth {
+    width: 60px;
+    height: 20px;
+    background: #840021;
+    top: 20%;
+    left: 14%;
+    border-radius: 50% / 0 0 100% 100%;
+  }
 
-/* page-ms */
-.page-ms {transform: translateY(-50px);}
+  .mouth::after{
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 13px solid #FFFFFF;
+    left: 10px;
+  }
 
-.error p.page-msg {
-	text-align: center;
-	color: #C0D7DD;
-	font-size: 30px;
-	font-family: 'Combo', cursive;
-	margin-bottom: 20px;
-}
-.go-back {
-	font-size: 30px;
+  .mouth::before{
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 13px solid #FFFFFF;
+    left: 40px;
+  }
+
+  .blod {
+    width: 8px;
+    height: 20px;
+    background: #840021;
+    top: 23%; left: 17%;
+    border-radius: 20px;
+  }
+
+  .blod::after{
+    width: 2px;
+    height: 10px;
+    background: #FFF;
+    top: 20%; left: 10%;
+    border-radius: 20px;
+  }
+
+  .blod2 {
+    top: 23%; left: 20%;
+    width: 13px;
+    height: 13px;
+    border-radius: 50% 50% 50% 0;
+    transform: rotate(130deg);
+    animation: blod 2s linear infinite;
+    opacity: 0;
+  }
+
+  @keyframes blod {
+    0%{
+      opacity: 1;
+    }
+    100%{
+      background:red;
+      opacity: 0;
+      top:50%;
+    }
+  }
+
+  /* page-ms */
+  .page-ms {
+    transform: translateY(-50px);
+  }
+
+  .error p.page-msg {
+    text-align: center;
+    color: #C0D7DD;
+    font-size: 30px;
+    font-family: 'Combo', cursive;
+    margin-bottom: 20px;
+  }
+
+  .go-back {
+    font-size: 30px;
     font-family: 'Combo', cursive;
     border: none;
     padding: 10px 20px;
@@ -209,8 +218,10 @@ body{
     background: #C0D7DD;
     color: #b63c06;
     box-shadow: 0 0 10px 0 #C0D7DD;
-	margin-top: 18px;
-}
+    margin-top: 18px;
+  }
 
-  button:hover {box-shadow: 0 0 20px 0 #C0D7DD;}
+  button:hover {
+    box-shadow: 0 0 20px 0 #C0D7DD;
+  }
 </style>
